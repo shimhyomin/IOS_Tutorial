b665f99da47b1a888e637c303d869e16abd2405e
shimhyomin <shimhm03@gmail.com>
2021-08-09 00:10:59 +0900

    Add LottieAnimation file install cocoapods and lottie

---
+++ LottieAnimation/LottieAnimation/ViewControllers/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LottieAnimation
+//
+//  Created by shm on 2021/08/09.
+//
+
+import UIKit
+import Lottie
+
+class MainViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
